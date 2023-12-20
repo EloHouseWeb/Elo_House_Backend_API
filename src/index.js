2a258e5b12386a82c27104b1215f7fcf1dbc6b3e
EloHouseWeb <elohouse.web@gmail.com>
2023-12-20 15:49:35 +0530

    base setup with DB

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+//const route = require("./routes/routes.js"); //imported route
+const mongoose = require("mongoose");
+require('dotenv').config()
+const app = express();
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const DB_uri = process.env.MONGODB_URI
+mongoose
+  .connect(DB_uri, {useNewUrlParser: true,})
+  .then(() => console.log("mongoDB Connected"))
+  .catch((err) => console.log(err));
+
+//app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});
