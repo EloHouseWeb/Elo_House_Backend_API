937c7e847c0be2a0a1afe60046ff1e52aa3f8cf4
EloHouseWeb <elohouse.web@gmail.com>
2024-01-03 09:02:00 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const route = require("./routes/routes.js"); //imported route
 const mongoose = require("mongoose");
 require('dotenv').config()
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
